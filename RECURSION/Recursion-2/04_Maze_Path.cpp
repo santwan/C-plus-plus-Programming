876f1ff08eac8a6df6c8756a347cca191817c117
santwan <santwanpathak1@gmail.com>
2023-12-19 18:35:42 +0530

    Add a Maze path question
    Adding a new question

--- RECURSION/Recursion-2/04_Maze_Path.cpp
+++ RECURSION/Recursion-2/04_Maze_Path.cpp
@@ -1,4 +1,20 @@
 /*
 SEE PAGE NO : 16
 */
- 
+
+#include<iostream>
+using namespace std;
+
+int maze(int sr , int sc , int er, int ec) {
+  if( sr > er || sc > ec ) return 0;
+  if( sr == er && sc == ec) return 1;
+  int rightway = maze(sr, sc+1, er, ec);
+  int downway = maze(sr+1,sc,er,ec);
+  int totalway = rightway+downway;
+  return totalway;
+}
+
+
+int main(){
+  cout<<maze(1,1,3,3);
+}
