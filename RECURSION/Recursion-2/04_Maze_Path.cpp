6a2d5a1790b5d643f1a5079d6b5de9ef65b1d72b
santwan <santwanpathak1@gmail.com>
2023-12-19 19:03:16 +0530

    Code modified
    I create a new function named as MazePath which will shows the possible paths to reach the destinatiopn.

--- RECURSION/Recursion-2/04_Maze_Path.cpp
+++ RECURSION/Recursion-2/04_Maze_Path.cpp
@@ -5,16 +5,29 @@ SEE PAGE NO : 16
 #include<iostream>
 using namespace std;
 
-int maze(int sr , int sc , int er, int ec) {
+int printMazePath(int sr , int sc , int er, int ec) {
   if( sr > er || sc > ec ) return 0;
   if( sr == er && sc == ec) return 1;
-  int rightway = maze(sr, sc+1, er, ec);
-  int downway = maze(sr+1,sc,er,ec);
+  int rightway = printMazePath(sr, sc+1, er, ec);
+  int downway = printMazePath(sr+1,sc,er,ec);
   int totalway = rightway+downway;
   return totalway;
 }
 
+//If you want to print the path also
+
+
+void MazePath(int sr , int sc , int er , int ec, string s){
+  if(sr > er || sc > ec) return;
+  if(sr==er && sc==ec){
+    cout<<s<<endl;
+    return;
+  }
+  MazePath(sr,sc+1,er,ec,s+"R");
+  MazePath(sr+1,sc,er,ec,s+"D");
+}
 
 int main(){
-  cout<<maze(1,1,3,3);
+  cout<<printMazePath(1,1,3,3)<<endl;
+  MazePath(1,1,5,5,"");
 }
