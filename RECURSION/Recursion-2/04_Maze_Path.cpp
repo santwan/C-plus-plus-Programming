f4eba3bcac1210707014ab4c32566dde42403e7d
santwan <santwanpathak1@gmail.com>
2023-12-20 10:13:54 +0530

    Added a variable and slight correction

--- RECURSION/Recursion-2/04_Maze_Path.cpp
+++ RECURSION/Recursion-2/04_Maze_Path.cpp
@@ -5,11 +5,11 @@ SEE PAGE NO : 16
 #include<iostream>
 using namespace std;
 
-int printMazePath(int sr , int sc , int er, int ec) {
+int Maze(int sr , int sc , int er, int ec) {
   if( sr > er || sc > ec ) return 0;
   if( sr == er && sc == ec) return 1;
-  int rightway = printMazePath(sr, sc+1, er, ec);
-  int downway = printMazePath(sr+1,sc,er,ec);
+  int rightway = Maze(sr, sc+1, er, ec);
+  int downway = Maze(sr+1,sc,er,ec);
   int totalway = rightway+downway;
   return totalway;
 }
@@ -28,6 +28,7 @@ void MazePath(int sr , int sc , int er , int ec, string s){
 }
 
 int main(){
-  cout<<printMazePath(1,1,3,3)<<endl;
-  MazePath(1,1,5,5,"");
+  int n=5;
+  cout<<Maze(1,1,n,n)<<endl;
+  MazePath(1,1,n,n,"");
 }
